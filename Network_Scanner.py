925f8b1a288f43d58f0c9d7114a02313638b7682
Henrique Curi <henriquecuridemiranda@hotmail.com>
2023-05-07 03:18:28 -0300

    add help

--- Network_Scanner.py
+++ Network_Scanner.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 
 import ARP_Request
+import optparse
 
-ip= str(input("IP address or Network to scan: "))
+parser=optparse.OptionParser()
+
+parser.add_option("-t", "--target", dest="ip", default="127.0.0.1", help="set IP address or network range (Model: x.x.x.x or x.x.x.x/x)")
+
+(options, arguments) = parser.parse_args()
+
+ip=str(options.ip).strip()
+
 ARP_Request.request(ip)
