7711744dc8824e8926c2c6cfbcb10d7d0a99d54c
Henrique Curi <henriquecuridemiranda@hotmail.com>
2023-05-07 03:56:34 -0300

    change from optparse to argparse

--- Network_Scanner.py
+++ Network_Scanner.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
 import ARP_Request
-import optparse
+import argparse
 
-parser=optparse.OptionParser()
+parser=argparse.ArgumentParser()
 
-parser.add_option("-t", "--target", dest="ip", default="127.0.0.1", help="set IP address or network range (Model: x.x.x.x or x.x.x.x/x)")
+parser.add_argument("-t", "--target", dest="ip", default="127.0.0.1", help="set IP address or network range (Model: x.x.x.x or x.x.x.x/x)")
 
-(options, arguments) = parser.parse_args()
+options = parser.parse_args()
 
 ip=str(options.ip).strip()
 
